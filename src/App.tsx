51c911a619f38cb7b5d4bee6333d140ae03e5fc9
MEGBLETO <carnelilordwiz@gmail.com>
2022-05-05 21:14:50 +0200

    first commit app setup plus tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className='text-4xl bg-slate-600'>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
